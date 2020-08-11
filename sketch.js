53c400f62c0fc9dad3cd67fd891b48c9237fcbd6
satvik9090 <64899312+satvik9090@users.noreply.github.com>
2020-08-11 22:40:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+var car,wall;
+var speed,weight;
+
+
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
+
+speed=random(55,90);
+weight+random(400,1500);
+
+
+car=createSprite(50,200,50,50);
+wall=createSprite(1500,20,60,height/2)
+
+car.velocityX= speed;
